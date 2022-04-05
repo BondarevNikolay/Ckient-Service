cc3b73af4d3791ce7876073309888303460cbc05
BondarevNikolay <draugort256yossi512@yandex.ru>
2022-04-05 19:57:17 +0300

    Add files via upload
    Client-server

---
+++ client/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.Client;
+
+namespace client
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var client = new JsonServiceClient("http://127.0.0.1:8080");
+
+            HelloResponse response = client.Get<HelloResponse>(new HelloRequest() { Name = "iPhone" });
+            //resultLabel.Text = response.Result;
+
+            Console.WriteLine(response.Result.ToString());
+        }
+    }
+}
